7b27444bae8319306bb15acfb791f62eef7ce0de
Bhimavarapupriyanka5 <146184376+Bhimavarapupriyanka5@users.noreply.github.com>
2024-07-20 11:50:29 +0530

    Delete Javascript directory

--- Javascript/Array.js
+++
@@ -1,9 +0,0 @@
-const firstNames =["priya","Mouni"];
-const lastName =["bh"];
-const newArray =[];
-for(let i=0; i<firstNames.length; i++){
-    let fullName =`${firstNames[i]} ${lastName}`;
-    newArray.push(fullName);
-}
-console.log(firstNames);
-console.log(newArray);
